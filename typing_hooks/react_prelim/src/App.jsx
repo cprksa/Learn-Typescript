f07a40b97cb2e971de385a7c031d2c77462a9e4a
Chan <chan.prksa@gmail.com>
2026-07-26 19:08:55 +0000

    Section: Building a stopwatch
    Details: Without a stop button

--- typing_hooks/react_prelim/src/App.jsx
+++ typing_hooks/react_prelim/src/App.jsx
@@ -1,12 +1,29 @@
-import { useRef } from "react";
+import { useState } from "react";
 
-export default function Counter() {
-  const ref = useRef(0);
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
 
-  function handleClick() {
-    ref.current = ref.current + 1;
-    alert("You clicked my Butt' " + ref.current + " times!");
+  function handleStart() {
+    // Start counting.
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    setInterval(() => {
+      // Update the current time every 10ms.
+      setNow(Date.now());
+    }, 10);
   }
 
-  return <button onClick={handleClick}>Click me!</button>;
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  return (
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+    </>
+  );
 }
