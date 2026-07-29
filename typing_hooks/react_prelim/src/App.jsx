a69e2d1d744f763e801a6f1f61150c15291277d7
Chan <chan.prksa@gmail.com>
2026-07-29 18:29:18 +0000

    Section: Flushing state updates synchronously with flushSync
    Code with a bug

--- typing_hooks/react_prelim/src/App.jsx
+++ typing_hooks/react_prelim/src/App.jsx
@@ -1,66 +1,38 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
-export default function CatFriends() {
-  const firstCatRef = useRef(null);
-  const secondCatRef = useRef(null);
-  const thirdCatRef = useRef(null);
+export default function TodoList() {
+  const listRef = useRef(null);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState(initialTodos);
 
-  function handleScrollToFirstCat() {
-    firstCatRef.current.scrollIntoView({
+  function handleAdd() {
+    const newTodo = { id: nextId++, text: text };
+    setText("");
+    setTodos([...todos, newTodo]);
+    listRef.current.lastChild.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
-      inline: "center",
-    });
-  }
-
-  function handleScrollToSecondCat() {
-    secondCatRef.current.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
-    });
-  }
-
-  function handleScrollToThirdCat() {
-    thirdCatRef.current.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
     });
   }
 
   return (
     <>
-      <nav>
-        <button onClick={handleScrollToFirstCat}>Neo</button>
-        <button onClick={handleScrollToSecondCat}>Millie</button>
-        <button onClick={handleScrollToThirdCat}>Bella</button>
-      </nav>
-      <div>
-        <ul>
-          <li>
-            <img
-              src="https://placecats.com/neo/300/200"
-              alt="Neo"
-              ref={firstCatRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placecats.com/millie/200/200"
-              alt="Millie"
-              ref={secondCatRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placecats.com/bella/199/200"
-              alt="Bella"
-              ref={thirdCatRef}
-            />
-          </li>
-        </ul>
-      </div>
+      <button onClick={handleAdd}>Add</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <ul ref={listRef}>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </>
   );
+}
+
+let nextId = 0;
+let initialTodos = [];
+for (let i = 0; i < 20; i++) {
+  initialTodos.push({
+    id: nextId++,
+    text: "Todo #" + (i + 1),
+  });
 }
