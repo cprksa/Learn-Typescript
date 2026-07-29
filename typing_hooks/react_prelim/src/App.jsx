4a5eaa1610c4f86178626ffbe9dd6e1764a7f327
Chan <chan.prksa@gmail.com>
2026-07-29 17:22:49 +0000

    Section: Scrolling to an element

--- typing_hooks/react_prelim/src/App.jsx
+++ typing_hooks/react_prelim/src/App.jsx
@@ -1,16 +1,66 @@
 import { useRef } from "react";
 
-export default function Form() {
-  const inputRef = useRef(null);
+export default function CatFriends() {
+  const firstCatRef = useRef(null);
+  const secondCatRef = useRef(null);
+  const thirdCatRef = useRef(null);
 
-  function handleClick() {
-    inputRef.current.focus();
+  function handleScrollToFirstCat() {
+    firstCatRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
+  function handleScrollToSecondCat() {
+    secondCatRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
+  function handleScrollToThirdCat() {
+    thirdCatRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
   }
 
   return (
     <>
-      <input ref={inputRef} />
-      <button onClick={handleClick}>Focus the input</button>
+      <nav>
+        <button onClick={handleScrollToFirstCat}>Neo</button>
+        <button onClick={handleScrollToSecondCat}>Millie</button>
+        <button onClick={handleScrollToThirdCat}>Bella</button>
+      </nav>
+      <div>
+        <ul>
+          <li>
+            <img
+              src="https://placecats.com/neo/300/200"
+              alt="Neo"
+              ref={firstCatRef}
+            />
+          </li>
+          <li>
+            <img
+              src="https://placecats.com/millie/200/200"
+              alt="Millie"
+              ref={secondCatRef}
+            />
+          </li>
+          <li>
+            <img
+              src="https://placecats.com/bella/199/200"
+              alt="Bella"
+              ref={thirdCatRef}
+            />
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
