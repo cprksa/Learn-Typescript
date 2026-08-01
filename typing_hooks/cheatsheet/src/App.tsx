b5b1287644780d92b4546b5f9c73de64e1597c98
Chan <chan.prksa@gmail.com>
2026-08-01 16:04:44 +0000

    useRef: Option 1: DOM element ref

--- typing_hooks/cheatsheet/src/App.tsx
+++ typing_hooks/cheatsheet/src/App.tsx
@@ -1,15 +1,22 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useRef, useEffect } from "react";
 
-function DelayedEffect(props: { timerMs: number }) {
-  const { timerMs } = props;
+function Foo() {
+  // - If possible, prefer as specific as possible. For example, HTMLDivElement
+  //   is better than HTMLElement and way better than Element.
+  // - Technical-wise, this returns RefObject<HTMLDivElement>
+  const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      /* do stuff */
-    }, timerMs);
-  }, [timerMs]);
-  // now wrapped in curly braces
-  return null;
+    // Note that ref.current may be null. This is expected, because you may
+    // conditionally render the ref-ed element, or you may forget to assign it
+    if (!divRef.current) throw Error("divRef is not assigned");
+
+    // Now divRef.current is sure to be HTMLDivElement
+    /* doSomethingWith(divRef.current); */
+  });
+
+  // Give the ref to an element so React can manage it for you
+  return <div ref={divRef}>etc</div>;
 }
 
 export default function App() {
@@ -27,7 +34,7 @@ export default function App() {
     <div>
       <h1>Welcome to the Cheatsheet App</h1>
       <button onClick={() => memoizedCallback("Hello", 42)}>Click Me</button>
-      <DelayedEffect timerMs={1000} />
+      <Foo />
     </div>
   );
 }
