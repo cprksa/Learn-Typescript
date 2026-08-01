1ea7e246d0f0cbca0b4fc14edd0054560c71faa4
Chan <chan.prksa@gmail.com>
2026-08-01 16:46:28 +0000

    useRef: Option 2: Mutable value ref

--- typing_hooks/cheatsheet/src/App.tsx
+++ typing_hooks/cheatsheet/src/App.tsx
@@ -1,22 +1,26 @@
 import { useCallback, useRef, useEffect } from "react";
 
 function Foo() {
-  // - If possible, prefer as specific as possible. For example, HTMLDivElement
-  //   is better than HTMLElement and way better than Element.
-  // - Technical-wise, this returns RefObject<HTMLDivElement>
-  const divRef = useRef<HTMLDivElement>(null);
+  const intervalRef = useRef<number | null>(null);
 
   useEffect(() => {
-    // Note that ref.current may be null. This is expected, because you may
-    // conditionally render the ref-ed element, or you may forget to assign it
-    if (!divRef.current) throw Error("divRef is not assigned");
+    intervalRef.current = window.setInterval(() => {
+      /* ... */
+    }, 1000);
+    return () => {
+      if (intervalRef.current !== null) clearInterval(intervalRef.current);
+    };
+  }, []);
 
-    // Now divRef.current is sure to be HTMLDivElement
-    /* doSomethingWith(divRef.current); */
-  });
-
-  // Give the ref to an element so React can manage it for you
-  return <div ref={divRef}>etc</div>;
+  return (
+    <button
+      onClick={() => {
+        /* clearInterval the ref */
+      }}
+    >
+      Cancel timer
+    </button>
+  );
 }
 
 export default function App() {
